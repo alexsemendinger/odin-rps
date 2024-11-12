60091693ba08f30c71ef46848abbbd74ecc18a67
alexsemendinger <66119933+alexsemendinger@users.noreply.github.com>
2024-11-12 11:10:52 -0500

    added game-over condition

--- rps.js
+++ rps.js
@@ -24,9 +24,11 @@ function playRound(humanChoice, computerChoice) {
     } else {
         roundTextField.textContent += " Draw!";
     }
+    checkIfGameOver();
 }
 
 function startRound(humanChoice) {
+    if (gameOver) return null;
     const computerChoice = getComputerChoice();
     describeRoundStart(humanChoice, computerChoice);
     playRound(humanChoice, computerChoice);
@@ -41,6 +43,23 @@ function describeRoundStart(humanChoice, computerChoice) {
     roundTextField.textContent = `${humanChoice} vs ${computerChoice}...`
     console.log(`${humanChoice} vs ${computerChoice}...`)
 }
+
+function checkIfGameOver() {
+    const humanScore = Number(humanScoreField.textContent);
+    const computerScore = Number(computerScoreField.textContent);
+    if (humanScore >=5 || computerScore >= 5) {
+        gameOver = true;
+    }
+    if (gameOver) {
+        const winner = (humanScore > computerScore) ? "Human" : "Computer";
+        const gameOverMessage = document.createElement("h3");
+        gameOverMessage.textContent = `Game over! Winner: ${winner}!`
+        rockButton.parentElement.remove(); // get rid of all buttons
+        computerScoreField.parentElement.appendChild(gameOverMessage);
+    }
+}
+
+let gameOver = false;
 
 const rockButton = document.querySelector("#rock")
 const paperButton = document.querySelector("#paper")
