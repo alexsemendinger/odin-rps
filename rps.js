dd293d72096a53c9942c67611b2b0e9ebb46ca95
alexsemendinger <66119933+alexsemendinger@users.noreply.github.com>
2024-11-12 10:53:44 -0500

    added interactivity, removed most console.logs

--- rps.js
+++ rps.js
@@ -1,96 +1,56 @@
 function getComputerChoice() {
-    let randVal = Math.random() * 3;
-    let choice = (randVal > 2) ? "rock" : ((randVal > 1) ? "paper" : "scissors");
-    return choice;  
+    return ["Rock", "Paper", "Scissors"][Math.floor(Math.random() * 3)] 
 }
 
-function getHumanChoice() {
-    let choice = prompt("Enter 'Rock', 'Paper', or 'Scissors'");
-    choice = choice.toLowerCase();
-    while (choice != "rock" && choice != "paper" && choice != "scissors") {
-        choice = prompt("I didn't recognize that option, try again.\nEnter 'Rock', 'Paper', or 'Scissors'");
-        choice = choice.toLowerCase();
-    }
-    return choice
+function scoreGame(choice1, choice2) {
+    // returns 1 if choice1 wins, -1 if choice1 loses, 0 if draw
+    if (choice1 === choice2) return 0;
+    const beats = { "Rock" : "Scissors", 
+                    "Paper" : "Rock",
+                    "Scissors" : "Paper" };
+    if (beats[choice1] == choice2) return 1;
+    return -1;
 }
 
 function playRound(humanChoice, computerChoice) {
-    console.log(`You chose ${humanChoice}!`);
-    console.log(`The computer chose ${computerChoice}!`);
-    if (humanChoice == computerChoice) {
-        console.log(`...Draw!`);
+    const result = scoreGame(humanChoice, computerChoice);
+    if (result === 1) {
+        incrementScore(humanScoreField);
+        roundTextField.textContent += " Human wins!";
     }
-    else if (humanChoice == "rock") {
-        switch (computerChoice) {
-            case "paper":
-                console.log("...Computer wins!");
-                computerScore++;
-                break;
-            case "scissors":
-                console.log("...Human wins!");
-                humanScore++;
-                break;
-            default:
-                console.warn("Oops, this shouldn't have happened.")
-        }
-    }
-    else if (humanChoice == "paper") {
-        switch (computerChoice) {
-            case "scissors":
-                console.log("...Computer wins!");
-                computerScore++;
-                break;
-            case "rock":
-                console.log("...Human wins!");
-                humanScore++;
-                break;
-            default:
-                console.warn("Oops, this shouldn't have happened.");
-        }
-    }
-    else if (humanChoice == "scissors") {
-        switch (computerChoice) {
-            case "rock":
-                console.log("...Computer wins!");
-                computerScore++;
-                break;
-            case "paper":
-                console.log("...Human wins!");
-                humanScore++;
-                break;
-            default:
-                console.warn("Oops, this shouldn't have happened.");
-        }
-    }
-} 
-
-function playGame() {
-    let gamesPlayed = 0;
-    
-    console.log("⚡️⚡️ U L T I M A T E     🪨R🪨  📄P📄  ✂️S✂️     2 0 2 4 ⚡️⚡️");
-    console.log(` PREPARE YOURSELF for ${NUMBER_OF_GAMES} rounds of UNFORGETTABLE RPS \n`);
-
-    for (let i = 1; i <= NUMBER_OF_GAMES; i++ ) {
-        console.log(` R O U N D   ${i}   B E G I N  \n`);
-        humanChoice = getHumanChoice();
-        computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-        console.log(`  SCORE: HUMAN ${humanScore} -- COMPUTER ${computerScore}\n\n\n`);
-    }
-
-    if (humanScore == computerScore) { 
-        console.log("\n\nOVERALL RESULT: DRAW!!!");
-        return null;
+    else if (result === -1) {
+        incrementScore(computerScoreField);
+        roundTextField.textContent += " Computer wins!";
     } else {
-        let winner = (humanScore > computerScore) ? "human" : "computer";
-        console.log("\n\nAND THE WINNER IS ......");
-        console.log(`....... ${winner.toUpperCase()}!!!`);
-        return winner;
+        roundTextField.textContent += " Draw!";
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
-const NUMBER_OF_GAMES = 5;
+function startRound(humanChoice) {
+    const computerChoice = getComputerChoice();
+    describeRoundStart(humanChoice, computerChoice);
+    playRound(humanChoice, computerChoice);
+}
 
-playGame();
+function incrementScore(scoreField) {
+    oldScore = Number(scoreField.textContent);
+    scoreField.textContent = oldScore + 1; 
+}
+
+function describeRoundStart(humanChoice, computerChoice) {
+    roundTextField.textContent = `${humanChoice} vs ${computerChoice}...`
+    console.log(`${humanChoice} vs ${computerChoice}...`)
+}
+
+const rockButton = document.querySelector("#rock")
+const paperButton = document.querySelector("#paper")
+const scissorsButton = document.querySelector("#scissors")
+
+const roundTextField = document.querySelector("#round-text")
+
+const humanScoreField = document.querySelector("#human-score")
+const computerScoreField = document.querySelector("#computer-score")
+
+rockButton.addEventListener("click", () => startRound('Rock'))
+paperButton.addEventListener("click", () => startRound('Paper'))
+scissorsButton.addEventListener("click", () => startRound('Scissors'))
